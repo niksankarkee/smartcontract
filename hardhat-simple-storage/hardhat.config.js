4bf35442f93b075618cae123c6e259c391c520d5
Niksan Karkee <niksankarkee@Niksans-MacBook-Pro.local>
2023-06-18 14:59:48 +0900

    hardhat simple storage

--- hardhat-simple-storage/hardhat.config.js
+++ hardhat-simple-storage/hardhat.config.js
@@ -3,12 +3,15 @@ const { task } = require("hardhat/config");
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 require("./tasks/block-number");
+require("hardhat-gas-reporter");
+require("solidity-coverage");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 
 const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
 const SEPOLIA_PRIVATE_KEY = process.env.SEPOLIA_PRIVATE_KEY;
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+const COIN_MARKET_API_KEY = process.env.COIN_MARKET_CAP_API_KEY;
 
 module.exports = {
   defaultNetwork: "hardhat",
@@ -19,9 +22,21 @@ module.exports = {
       accounts: [SEPOLIA_PRIVATE_KEY],
       chainId: 11155111,
     },
+    localhost: {
+      url: "http://127.0.0.1:8545/",
+      chainId: 31337,
+    },
   },
   solidity: "0.8.18",
   etherscan: {
     apiKey: ETHERSCAN_API_KEY,
+  },
+  gasReporter: {
+    enabled: true,
+    outputFile: "gas-report.txt",
+    noColors: true,
+    currency: "USD",
+    coinmarketcap: COIN_MARKET_API_KEY,
+    token: "MATIC",
   },
 };
